0441f1e98ed9198b4ca84b4f990c6e41d7ecd833
Gustavo Sobreira <137525185+sobreirinha@users.noreply.github.com>
2026-07-31 20:47:57 -0300

    Update séries.js

--- arquivos/séries.js
+++ arquivos/séries.js
@@ -1,1 +1,59 @@
+/* ==========================================
+   MY PERSONAL LIBRARY
+   Séries cadastradas
+========================================== */
 
+const series = [
+
+    {
+        id: 1,
+
+        titulo: "Rick and Morty",
+
+        categoria: "watching",
+
+        capa: "https://m.media-amazon.com/images/M/MV5BMmY2M2Y5YzQtODQ2Yy00ZDYwLWJkMzQtNTRkNjk2N2Q3ZjVjXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+
+        plataforma: "Max",
+
+        temporadas: 8,
+
+        episodios: 81,
+
+        assistidos: 52,
+
+        ultimaTemporada: 7,
+
+        ultimoEpisodio: 4,
+
+        favorita: true
+
+    },
+
+    {
+
+        id: 2,
+
+        titulo: "Avatar: O Último Mestre do Ar",
+
+        categoria: "watching",
+
+        capa: "https://m.media-amazon.com/images/M/MV5BNmQzODZjZGQtNjM0Mi00OGUxLWEwNmUtODQ2NjI5ZjE2M2M1XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+
+        plataforma: "Netflix",
+
+        temporadas: 1,
+
+        episodios: 8,
+
+        assistidos: 5,
+
+        ultimaTemporada: 1,
+
+        ultimoEpisodio: 5,
+
+        favorita: false
+
+    }
+
+];
